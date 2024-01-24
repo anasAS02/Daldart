6e1b0081e321d3ae2572263fe67264c3a16bbed6
Anas <110332632+anasAS02@users.noreply.github.com>
2024-01-25 00:28:42 +0200

    Edit Navbar

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,10 +1,12 @@
-
 export default function Navbar(){
     return(
-        <div className='w-full p-5 flex items-center justify-center gap-8 bg-zinc-900 text-white'>
-            <span className='cursor-pointer delay-200 hover:text-yellow-500'>Hot</span>
-            <span className='cursor-pointer delay-200 hover:text-yellow-500'>New</span>
-            <span className='cursor-pointer delay-200 hover:text-yellow-500'>Rising</span>
+        <div className='w-full p-5 flex justify-center gap-10 items-center bg-zinc-900'>
+            <div className='categories flex items-center gap-4 text-white'>
+                <span className='cursor-pointer delay-200 hover:text-yellow-500'>Hot</span>
+                <span className='cursor-pointer delay-200 hover:text-yellow-500'>New</span>
+                <span className='cursor-pointer delay-200 hover:text-yellow-500'>Raising</span>
+            </div>
+            <input type='search' placeholder='Search...' className='w-50 bg-zinc-800 p-3 rounded-2xl focus:w-96 outline-none' />
         </div>
     )
 }
