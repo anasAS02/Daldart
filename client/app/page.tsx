a1c5404391484ef7da79be5cea93ed14f28e33ba
Anas <110332632+anasAS02@users.noreply.github.com>
2024-01-25 12:27:11 +0200

    Add a global state for posts

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,8 @@
 'use client'
 import Navbar from "./components/Navbar";
 import Posts from "./components/Posts";
-import { useStatusContext } from "./utils/globalStatus";
 
 export default function Home() {
-  const {isLoading, listing} = useStatusContext();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar />
--- client/app/utils/globalStatus.tsx
+++ client/app/utils/globalStatus.tsx
@@ -6,21 +6,26 @@ interface contextProps {
     setListing: Dispatch<SetStateAction<string>>,
     isLoading: boolean,
     setIsLoading: Dispatch<SetStateAction<boolean>>,
+    postsArr: any[],
+    setPostsArr: Dispatch<SetStateAction<any[]>>,
 }
 
 const StatusContext = createContext<contextProps>({
     listing: 'hot',
     setListing: (): string => 'hot',
     isLoading: false,
-    setIsLoading: (): boolean => false
+    setIsLoading: (): boolean => false,
+    postsArr: [],
+    setPostsArr: (): any[] => []
 });
 
 export const StatusContextProvider = ({ children }: any) => {
     const [listing, setListing] = useState<string>('hot');
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [postsArr, setPostsArr] = useState<any[]>([]);
 
     return(
-        <StatusContext.Provider value={{listing, setListing, isLoading, setIsLoading}}>
+        <StatusContext.Provider value={{listing, setListing, isLoading, setIsLoading, postsArr, setPostsArr}}>
             { children }
         </StatusContext.Provider>
     )
