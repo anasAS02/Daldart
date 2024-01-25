b803744ccabd8136d5c9b18a1f0772bca17a4710
Anas <110332632+anasAS02@users.noreply.github.com>
2024-01-25 13:53:13 +0200

    Add scrollToTop function

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,20 @@
 'use client'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Navbar from "./components/Navbar";
 import Posts from "./components/Posts";
+import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { useStatusContext } from "./utils/globalStatus";
 
 export default function Home() {
+  const {isLoading} = useStatusContext();
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+}
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="relative flex min-h-screen flex-col items-center justify-between">
       <Navbar />
       <Posts />
+      {!isLoading && <FontAwesomeIcon onClick={scrollToTop} icon={faCircleArrowUp} className='absolute right-5 bottom-0 max-md:w-[30px] max-md:h-[30px] w-[60px] h-[60px] cursor-pointer duration-300 hover:text-yellow-500 animate-bounce' />}
     </main>
   );
 }
