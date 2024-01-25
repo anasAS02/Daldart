d9a62f71523341827f59c43ab56a697131937c7f
Anas <110332632+anasAS02@users.noreply.github.com>
2024-01-25 14:04:34 +0200

    Add footer

---
+++ client/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer className='w-full p-5 bg-zinc-900 text-white text-center'>
+            <p>Copyrights reserved to <span className='text-red-500'>Anas</span> - 2024 &copy;</p>
+        </footer>
+    )
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Posts from "./components/Posts";
 import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { useStatusContext } from "./utils/globalStatus";
+import Footer from "./components/Footer";
 
 export default function Home() {
   const {isLoading} = useStatusContext();
@@ -14,7 +15,8 @@ export default function Home() {
     <main className="relative flex min-h-screen flex-col items-center justify-between">
       <Navbar />
       <Posts />
-      {!isLoading && <FontAwesomeIcon onClick={scrollToTop} icon={faCircleArrowUp} className='absolute right-5 bottom-0 max-md:w-[30px] max-md:h-[30px] w-[60px] h-[60px] cursor-pointer duration-300 hover:text-yellow-500 animate-bounce' />}
+      {!isLoading && <FontAwesomeIcon onClick={scrollToTop} icon={faCircleArrowUp} className='absolute right-5 bottom-20 max-md:w-[30px] max-md:h-[30px] w-[60px] h-[60px] cursor-pointer duration-300 hover:text-yellow-500 animate-bounce' />}
+      <Footer />
     </main>
   );
 }
